f01ec00496492c1a42291b544e4b4d1e82006003
unknown <delkanoberutu8@gmail.com>
2026-05-13 11:58:49 +0700

    feat: implement hover system for main menu icons

--- Pulih/Assets/Scripts/Menu/MainMenuController.cs
+++ Pulih/Assets/Scripts/Menu/MainMenuController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -8,9 +10,23 @@ public class MainMenuController : MonoBehaviour
     public GameObject game;
 
     [SerializeField] private CamerController cameraController;
+    [SerializeField] private Image[] hoverImages;
+    
+    private TextMeshProUGUI[] buttonTexts;
+    private Color colorHover = new Color32(226, 214, 192, 255);
+    private Color colorNormal = new Color32(152, 141, 122, 255);
 
     void Start()
     {
+        buttonTexts = new TextMeshProUGUI[hoverImages.Length];
+        for (int i = 0; i < hoverImages.Length; i++)
+        {
+            if (hoverImages[i] != null)
+            {
+                buttonTexts[i] = hoverImages[i].transform.parent.GetComponentInChildren<TextMeshProUGUI>();
+            }
+        }
+
         if (mainMenu != null) mainMenu.SetActive(true);
         if (loadingScene != null) loadingScene.SetActive(false);
         if (game != null) game.SetActive(false);
@@ -18,6 +34,26 @@ public class MainMenuController : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        UpdateHoverAlpha(0);
+    }
+
+    public void UpdateHoverAlpha(int index)
+    {
+        for (int i = 0; i < hoverImages.Length; i++)
+        {
+            if (hoverImages[i] != null)
+            {
+                Color tempColor = hoverImages[i].color;
+                tempColor.a = (i == index) ? 1f : 0f;
+                hoverImages[i].color = tempColor;
+
+                if (buttonTexts[i] != null)
+                {
+                    buttonTexts[i].color = (i == index) ? colorHover : colorNormal;
+                }
+            }
+        }
     }
 
     public void OnPlayButton()
@@ -38,5 +74,13 @@ public class MainMenuController : MonoBehaviour
 
         if (cameraController != null) cameraController.enabled = true;
         if (loadingScene != null) Destroy(loadingScene);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }

eb322fd4d3d3c4960d83f0801219b93621cf1e84
unknown <delkanoberutu8@gmail.com>
2026-05-18 21:58:43 +0700

    add pickup using trash grabber

---
+++ Pulih/Assets/Scripts/Chapters/Chapter 1/PickupUsingTrashGrabber.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using TMPro;
+
+public class PickupUsingTrashGrabber : MonoBehaviour
+{
+    public TMP_Text trashCounterText;
+    public int maxTrash = 5;
+    public InteractUiPosition uiPosition;
+    public SmokeAnimation smokeAnimation;
+    public Collider grabberCollider;
+    [HideInInspector] public int currentTrash = 0;
+    private PickupController pickupController;
+    private bool isHeld = false;
+
+    void Start()
+    {
+        if (uiPosition != null) uiPosition.SetUI(false);
+
+        UpdateUI();
+    }
+
+    void Update()
+    {
+        if (transform.parent != null)
+        {
+            PickupController foundController = GetComponentInParent<PickupController>();
+
+            if (foundController != null)
+            {
+                if (!isHeld)
+                {
+                    pickupController = foundController;
+                    isHeld = true;
+
+                    if (uiPosition != null)
+                    {
+                        uiPosition.SetUI(true);
+                    }
+                }
+            }
+            else
+            {
+                if (isHeld && uiPosition != null)
+                {
+                    uiPosition.SetUI(false);
+                }
+
+                isHeld = false;
+                pickupController = null;
+            }
+        }
+        else
+        {
+            if (isHeld && uiPosition != null)
+            {
+                uiPosition.SetUI(false);
+            }
+
+            isHeld = false;
+            pickupController = null;
+        }
+
+        if (isHeld && pickupController != null)
+        {
+            CheckHandForTrash(pickupController.rightHandTransform);
+            CheckHandForTrash(pickupController.leftHandTransform);
+        }
+    }
+
+    void CheckHandForTrash(Transform handTransform)
+    {
+        if (handTransform == null) return;
+
+        for (int i = handTransform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = handTransform.GetChild(i);
+
+            if (child.CompareTag("Trash"))
+            {
+                if (currentTrash < maxTrash)
+                {
+                    currentTrash++;
+
+                    UpdateUI();
+
+                    if (smokeAnimation != null)
+                    {
+                        smokeAnimation.PlaySmoke();
+                    }
+
+                    if (grabberCollider != null)
+                    {
+                        child.SetParent(grabberCollider.transform);
+                        child.localPosition = Vector3.zero;
+                    }
+                }
+            }
+        }
+    }
+    void UpdateUI()
+    {
+        if (trashCounterText != null)
+        {
+            trashCounterText.text = currentTrash + "/" + maxTrash;
+        }
+    }
+}

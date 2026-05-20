9f858240c643e48d94a7f6490b8d93cce44626ed
unknown <delkanoberutu8@gmail.com>
2026-05-20 15:23:51 +0700

    fix animation when animation value is null or empty

--- Pulih/Assets/Scripts/Pickup/PickupController.cs
+++ Pulih/Assets/Scripts/Pickup/PickupController.cs
@@ -95,6 +95,10 @@ public class PickupController : MonoBehaviour
             {
                 animator.SetBool(item.holdAnimation, true);
             }
+            else
+            {
+                animator.SetBool(item.holdAnimation, false);
+            }
         }
         else if (item.leftFirst)
         {
